b50301022697a889779b08013bbb98cc1a044c7a
igor <ijovin@conga.com>
2022-02-21 08:51:39 +0100

    Add conditional importing of boto3

--- app/config/connections.py
+++ app/config/connections.py
@@ -1,12 +1,13 @@
 from app.config.common import DEV_ENV, AWS_SERVER_PUBLIC_KEY, AWS_SERVER_SECRET_KEY, AWS_REGION_NAME
 
-import boto3
-import localstack_client.session as boto3_local
-
+if DEV_ENV:
+    import localstack_client.session as boto3
+else:
+    import boto3
 
 def get_aws_session():
     if DEV_ENV:
-        aws_session = boto3_local.Session(localstack_host="localstack-container")
+        aws_session = boto3.Session(localstack_host="localstack-container")
         return aws_session
     aws_session = boto3.Session(aws_access_key_id=AWS_SERVER_PUBLIC_KEY,
                                 aws_secret_access_key=AWS_SERVER_SECRET_KEY,
